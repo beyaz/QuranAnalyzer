d723dd39f3c0727268c12165d9c5983ade8be487
Abdullah Beyaztaş <beyaz1404@gmail.com>
2023-03-12 23:56:28 +0300

    .

--- QuranAnalyzer.WebUI/wwwroot/index.jsx
+++ QuranAnalyzer.WebUI/wwwroot/index.jsx
@@ -5,17 +5,11 @@ import ReactWithDotNet from "./react-with-dotnet/react-with-dotnet";
 
 // you can comment these imports according to your project dependency
 import "./react-with-dotnet/libraries/mui-core/all";
-import "./react-with-dotnet/libraries/google-map-react/all";
 import "./react-with-dotnet/libraries/primereact/all";
 import "./react-with-dotnet/libraries/react-awesome-reveal/all";
-import "./react-with-dotnet/libraries/react-datepicker/all";
 import "./react-with-dotnet/libraries/react-free-scrollbar/all";
 import "./react-with-dotnet/libraries/react-xarrows/all";
-import "./react-with-dotnet/libraries/rsuite/all";
-import "./react-with-dotnet/libraries/swiper/all";
-import "./react-with-dotnet/libraries/uiw-react-codemirror/all";
 import "./react-with-dotnet/libraries/framer-motion/all";
-
 
 var currentScrollY = 0;
 
