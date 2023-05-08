7ae9376f283110ec687da0897ffcbc7321a29da1
beyaz <beyaz1404@gmail.com>
2023-05-08 23:45:00 +0300

    .

--- QuranAnalyzer.WebUI/wwwroot/index.jsx
+++ QuranAnalyzer.WebUI/wwwroot/index.jsx
@@ -2,12 +2,10 @@
 
 // import core library
 import ReactWithDotNet from "./react-with-dotnet/react-with-dotnet";
-import "./react-with-dotnet/libraries/uiw-react-codemirror/all";
 import "./react-with-dotnet/libraries/react-free-scrollbar/all";
 
 // you can comment these imports according to your project dependency
 import "./react-with-dotnet/libraries/mui-core/all";
-import "./react-with-dotnet/libraries/primereact/all";
 import "./react-with-dotnet/libraries/react-awesome-reveal/all";
 import "./react-with-dotnet/libraries/react-free-scrollbar/all";
 import "./react-with-dotnet/libraries/react-xarrows/all";
