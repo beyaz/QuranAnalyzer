27f9aeb1a3f9d69d3fad0529d5da9251c505dc98
beyaz <beyaz1404@gmail.com>
2023-05-16 01:56:20 +0300

    .

--- QuranAnalyzer.WebUI/Components/SvgHamburgerIcon.cs
+++ QuranAnalyzer.WebUI/Components/SvgHamburgerIcon.cs
@@ -6,16 +6,11 @@ class SvgHamburgerIcon : ReactPureComponent
     {
         return new svg
         {
-            viewBox = "0 0 24 24",
-            children =
-            {
-                new path { d = "M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z", fill = Theme.TextColor }
-            },
-            style =
-            {
-                Height(24),
-                Margin(12)
-            }
+            svg.ViewBox(0, 0, 24, 24),
+            Height(24),
+            Margin(12),
+
+            new path { d = "M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z", fill = Theme.TextColor }
         };
     }
 }
