d5e620dd60b87bf469a941a6775080d9408c6486
beyaz <beyaz1404@gmail.com>
2023-05-16 01:25:40 +0300

    .

---
+++ QuranAnalyzer.WebUI/wwwroot/index-dev.jsx
@@ -0,0 +1,8 @@
+
+
+// import core library
+import "./index";
+
+// required for ReactWithDotNet designer
+import "./react-with-dotnet/libraries/primereact/all";
+import "./react-with-dotnet/libraries/uiw-react-codemirror/all";
