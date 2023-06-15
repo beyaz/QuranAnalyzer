4f09e6d8784b1347b343adf1e5dea6dd1bf0fa8a
beyaz <beyaz1404@gmail.com>
2023-06-15 20:37:39 +0300

    12

--- QuranAnalyzer.Tests/CustomCountingTests.cs
+++ QuranAnalyzer.Tests/CustomCountingTests.cs
@@ -36,44 +36,99 @@ public class CustomCountingTests
         var allLetters = string.Join(string.Empty, allCharachtersAsList);
 
         for (var i0 = 0; i0 < 28; i0++)
+        {
+            var v0 = allCharachtersNumericValuesAsList[i0];
+            var o0 = i0 + 1;
+            
             for (var i1 = 0; i1 < 28; i1++)
+            {
+                var v1 = allCharachtersNumericValuesAsList[i1];
+                var o1 = i1 + 1;
+                
+                var sum1 = v0 + v1;
+                var orderSum1 = o0 + o1;
+                
                 for (var i2 = 0; i2 < 28; i2++)
+                {
+                    var v2 = allCharachtersNumericValuesAsList[i2];
+                    var o2 = i2 + 1;
+
+                    var sum2 = sum1 + v2;
+                    var orderSum2 = orderSum1 + o2;
+                    
                     for (var i3 = 0; i3 < 28; i3++)
+                    {
+                        var v3 = allCharachtersNumericValuesAsList[i3];
+                        var o3 = i3 + 1;
+                        
+                        var sum3 = sum2 + v3;
+                        var orderSum3 = orderSum2 + o3;
+                        
                         for (var i4 = 0; i4 < 28; i4++)
+                        {
+                            var v4 = allCharachtersNumericValuesAsList[i4];
+                            var o4 = i4 + 1;
+
+                            var sum4 = sum3 + v4;
+                            var orderSum4 = orderSum3 + o4;
+                            
                             for (var i5 = 0; i5 < 28; i5++)
+                            {
+                                var v5 = allCharachtersNumericValuesAsList[i5];
+                                var o5 = i5 + 1;
+                                
+                                var sum5 = sum4 + v5;
+                                var orderSum5 = orderSum4 + o5;
+                                
                                 for (var i6 = 0; i6 < 28; i6++)
+                                {
+                                    var v6 = allCharachtersNumericValuesAsList[i6];
+                                    var o6 = i6 + 1;
+                                    
+                                    var sum6 = sum5 + v6;
+                                    var orderSum6 = orderSum5 + o6;
+                                    
                                     for (var i7 = 0; i7 < 28; i7++)
+                                    {
+                                        var v7 = allCharachtersNumericValuesAsList[i7];
+                                        var o7 = i7 + 1;
+                                        
+                                        var sum7 = sum6 + v7;
+                                        var orderSum7 = orderSum6 + o7;
+                                        
                                         for (var i8 = 0; i8 < 28; i8++)
+                                        {
+                                            var v8 = allCharachtersNumericValuesAsList[i8];
+                                            var o8 = i8 + 1;
+                                            
+                                            var sum8 = sum7 + v8;
+                                            var orderSum8 = orderSum7 + o8;
+                                            
                                             for (var i9 = 0; i9 < 28; i9++)
+                                            {
+                                                var v9 = allCharachtersNumericValuesAsList[i9];
+                                                var o9 = i9 + 1;
+                                                
+                                                var sum9 = sum8 + v9;
+                                                var orderSum9 = orderSum8 + o9;
+                                                
                                                 for (var i10 = 0; i10 < 28; i10++)
+                                                {
+                                                    var v10 = allCharachtersNumericValuesAsList[i10];
+                                                    var o10 = i10 + 1;
+                                                    
+                                                    
+                                                    var sum10 = sum9 + v10;
+                                                    var orderSum10 = orderSum9 + o10;
+                                                    
                                                     for (var i11 = 0; i11 < 28; i11++)
                                                     {
+                                                        var v11 = allCharachtersNumericValuesAsList[i11];
+                                                        var o11 = i11 + 1;
 
-                                                        var sum = allCharachtersNumericValuesAsList[i0] +
-                                                                  allCharachtersNumericValuesAsList[i1] +
-                                                                  allCharachtersNumericValuesAsList[i2] +
-                                                                  allCharachtersNumericValuesAsList[i3] +
-                                                                  allCharachtersNumericValuesAsList[i4] +
-                                                                  allCharachtersNumericValuesAsList[i5] +
-                                                                  allCharachtersNumericValuesAsList[i6] +
-                                                                  allCharachtersNumericValuesAsList[i7] +
-                                                                  allCharachtersNumericValuesAsList[i8] +
-                                                                  allCharachtersNumericValuesAsList[i9] +
-                                                                  allCharachtersNumericValuesAsList[i10] +
-                                                                  allCharachtersNumericValuesAsList[i11];
+                                                        var sum = sum10 + v11;
 
-                                                        var orderSum = i0 + 1 +
-                                                                       i1 + 1 +
-                                                                       i2 + 1 +
-                                                                       i3 + 1 +
-                                                                       i4 + 1 +
-                                                                       i5 + 1 +
-                                                                       i6 + 1 +
-                                                                       i7 + 1 +
-                                                                       i8 + 1 +
-                                                                       i9 + 1 +
-                                                                       i10 + 1 +
-                                                                       i11 + 1;
+                                                        var orderSum = orderSum10 + o11;
 
                                                         if (sum == 667 && orderSum == 109)
                                                         {
@@ -82,6 +137,28 @@ public class CustomCountingTests
 
                                                         numberOfProcessedItem++;
                                                     }
+                                                    
+                                                }
+                                                
+                                            }
+                                            
+                                        }
+                                        
+                                    }
+                                    
+                                }
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
                             
         
         
