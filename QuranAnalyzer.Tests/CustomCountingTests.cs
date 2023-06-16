f21e25416a7f42b231d7e162baa084a9dc5588c8
beyaz <beyaz1404@gmail.com>
2023-06-16 23:12:23 +0300

    .

--- QuranAnalyzer.Tests/CustomCountingTests.cs
+++ QuranAnalyzer.Tests/CustomCountingTests.cs
@@ -27,17 +27,17 @@ public class CustomCountingTests
             throw new Exception("wrong input");
         }
 
-        const int maxOccurenceLength = 6;
+        const int arrayLength = 28;
 
         BigInteger numberOfProcessedItem = 0;
         BigInteger numberOfFounds = 0;
 
-        for (var i0 = 0; i0 < maxOccurenceLength; i0++)
+        for (var i0 = 0; i0 < arrayLength; i0++)
         {
             var v0 = allCharachtersNumericValuesAsList[i0];
             var o0 = i0 + 1;
 
-            for (var i1 = 0; i1 < maxOccurenceLength; i1++)
+            for (var i1 = 0; i1 < arrayLength; i1++)
             {
                 var v1 = allCharachtersNumericValuesAsList[i1];
                 var o1 = i1 + 1;
@@ -45,7 +45,7 @@ public class CustomCountingTests
                 var sum1 = v0 + v1;
                 var orderSum1 = o0 + o1;
 
-                for (var i2 = 0; i2 < maxOccurenceLength; i2++)
+                for (var i2 = 0; i2 < arrayLength; i2++)
                 {
                     var v2 = allCharachtersNumericValuesAsList[i2];
                     var o2 = i2 + 1;
@@ -53,7 +53,7 @@ public class CustomCountingTests
                     var sum2 = sum1 + v2;
                     var orderSum2 = orderSum1 + o2;
 
-                    for (var i3 = 0; i3 < maxOccurenceLength; i3++)
+                    for (var i3 = 0; i3 < arrayLength; i3++)
                     {
                         var v3 = allCharachtersNumericValuesAsList[i3];
                         var o3 = i3 + 1;
@@ -61,7 +61,7 @@ public class CustomCountingTests
                         var sum3 = sum2 + v3;
                         var orderSum3 = orderSum2 + o3;
 
-                        for (var i4 = 0; i4 < maxOccurenceLength; i4++)
+                        for (var i4 = 0; i4 < arrayLength; i4++)
                         {
                             var v4 = allCharachtersNumericValuesAsList[i4];
                             var o4 = i4 + 1;
@@ -69,7 +69,7 @@ public class CustomCountingTests
                             var sum4 = sum3 + v4;
                             var orderSum4 = orderSum3 + o4;
 
-                            for (var i5 = 0; i5 < maxOccurenceLength; i5++)
+                            for (var i5 = 0; i5 < arrayLength; i5++)
                             {
                                 var v5 = allCharachtersNumericValuesAsList[i5];
                                 var o5 = i5 + 1;
@@ -77,7 +77,7 @@ public class CustomCountingTests
                                 var sum5 = sum4 + v5;
                                 var orderSum5 = orderSum4 + o5;
 
-                                for (var i6 = 0; i6 < maxOccurenceLength; i6++)
+                                for (var i6 = 0; i6 < arrayLength; i6++)
                                 {
                                     var v6 = allCharachtersNumericValuesAsList[i6];
                                     var o6 = i6 + 1;
@@ -85,7 +85,7 @@ public class CustomCountingTests
                                     var sum6 = sum5 + v6;
                                     var orderSum6 = orderSum5 + o6;
 
-                                    for (var i7 = 0; i7 < maxOccurenceLength; i7++)
+                                    for (var i7 = 0; i7 < arrayLength; i7++)
                                     {
                                         var v7 = allCharachtersNumericValuesAsList[i7];
                                         var o7 = i7 + 1;
@@ -93,7 +93,7 @@ public class CustomCountingTests
                                         var sum7 = sum6 + v7;
                                         var orderSum7 = orderSum6 + o7;
 
-                                        for (var i8 = 0; i8 < maxOccurenceLength; i8++)
+                                        for (var i8 = 0; i8 < arrayLength; i8++)
                                         {
                                             var v8 = allCharachtersNumericValuesAsList[i8];
                                             var o8 = i8 + 1;
@@ -101,7 +101,7 @@ public class CustomCountingTests
                                             var sum8 = sum7 + v8;
                                             var orderSum8 = orderSum7 + o8;
 
-                                            for (var i9 = 0; i9 < maxOccurenceLength; i9++)
+                                            for (var i9 = 0; i9 < arrayLength; i9++)
                                             {
                                                 var v9 = allCharachtersNumericValuesAsList[i9];
                                                 var o9 = i9 + 1;
@@ -109,7 +109,7 @@ public class CustomCountingTests
                                                 var sum9 = sum8 + v9;
                                                 var orderSum9 = orderSum8 + o9;
 
-                                                for (var i10 = 0; i10 < maxOccurenceLength; i10++)
+                                                for (var i10 = 0; i10 < arrayLength; i10++)
                                                 {
                                                     var v10 = allCharachtersNumericValuesAsList[i10];
                                                     var o10 = i10 + 1;
@@ -117,7 +117,7 @@ public class CustomCountingTests
                                                     var sum10 = sum9 + v10;
                                                     var orderSum10 = orderSum9 + o10;
 
-                                                    for (var i11 = 0; i11 < maxOccurenceLength; i11++)
+                                                    for (var i11 = 0; i11 < arrayLength; i11++)
                                                     {
                                                         var v11 = allCharachtersNumericValuesAsList[i11];
                                                         var o11 = i11 + 1;
