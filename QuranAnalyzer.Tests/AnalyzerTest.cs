ef4a4c1f3dec93d8d34eaddc03d9fe13342fc373
beyaz <beyaz1404@gmail.com>
2023-06-17 11:30:35 +0300

    .

--- QuranAnalyzer.Tests/AnalyzerTest.cs
+++ QuranAnalyzer.Tests/AnalyzerTest.cs
@@ -16,6 +16,14 @@ public class AnalyzerTest
         Analyzer.AnalyzeText("يَٰصَىٰحِبَىِ ٱلسِّجْنِ أَمَّآ أَحَدُكُمَا فَيَسْقِى رَبَّهُۥ خَمْرًا وَأَمَّا ٱلْءَاخَرُ فَيُصْلَبُ فَتَأْكُلُ ٱلطَّيْرُ مِن رَّأْسِهِۦ قُضِىَ ٱلْأَمْرُ ٱلَّذِى فِيهِ تَسْتَفْتِيَانِ")
                 .Count(x => x.ArabicLetterIndex == ArabicLetterIndex.Yaa)
                 .Should().Be(11);
+        
+        
+        "يَٰصَىٰحِبَىِ".Select((c, i) => Calculator.GetLetterInfo(c, i, true)).Select(x => new LetterInfo
+        {
+            ArabicLetterIndex = x.ArabicLetterIndex,
+            MatchedLetter     = x.MatchedLetter.ToString(),
+            StartIndex        = x.StartIndex
+        }).Count(x => x.ArabicLetterIndex == ArabicLetterIndex.Yaa).Should().Be(3);
     }
 
     //[TestMethod]
