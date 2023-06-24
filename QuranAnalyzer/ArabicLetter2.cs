63d0049bc6bfe13ee9198e8bd6f6b6d488f13d47
beyaz <beyaz1404@gmail.com>
2023-06-24 09:44:14 +0300

    .

--- QuranAnalyzer/ArabicLetter2.cs
+++ QuranAnalyzer/ArabicLetter2.cs
@@ -33,3 +33,37 @@ public static class ArabicLetter2
     public const char Ghayn = 'غ';
     // @formatter:on
 }
+
+public static class ArabicLetterOrder
+{
+    // @formatter:off
+    public const int Alif  = 1;
+    public const int Baa   = 2;
+    public const int Jiim  = 3;
+    public const int Daal  = 4;
+    public const int Haa_  = 5;
+    public const int Waaw  = 6;
+    public const int Zay   = 7;
+    public const int Haa   = 8;
+    public const int Taa_  = 9;
+    public const int Yaa   = 10;
+    public const int Kaaf  = 11;
+    public const int Laam  = 12;
+    public const int Miim  = 13;
+    public const int Nun   = 14;
+    public const int Siin  = 15;
+    public const int Ayn   = 16;
+    public const int Faa   = 17;
+    public const int Saad  = 18;
+    public const int Qaaf  = 19;
+    public const int Raa   = 20;
+    public const int Shiin = 21;
+    public const int Taa   = 22;
+    public const int Thaa  = 23;
+    public const int Khaa  = 24;
+    public const int Dhaal = 25;
+    public const int Daad  = 26;
+    public const int Zaa   = 27;
+    public const int Ghayn = 28;
+    // @formatter:on
+}
