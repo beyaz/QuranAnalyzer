7fc49b2a9ee7b2a82954f7d25bf60b68ed3067c2
beyaz <beyaz1404@gmail.com>
2023-10-06 23:02:27 +0300

    .

---
+++ QuranAnalyzer/QuranArabicVersion.cs
@@ -0,0 +1,25 @@
+﻿namespace QuranAnalyzer;
+
+partial class QuranArabicVersionWithNoBismillah 
+{
+    public static (bool isParsedSuccessfully, int grandVerseNumber, int chapterNumber, int verseNumber, string verseText) TryParseVerseNumbers(string verseText)
+    {
+        var arr = verseText.Split('|', StringSplitOptions.RemoveEmptyEntries);
+        if (arr.Length == 4)
+        {
+            if (int.TryParse(arr[0], out var grandVerseNumber))
+            {
+                if (int.TryParse(arr[1], out var chapterNumber))
+                {
+                    if (int.TryParse(arr[2], out var verseNumber))
+                    {
+                        return (isParsedSuccessfully: true, grandVerseNumber, chapterNumber, verseNumber, verseText: arr[3]);
+                    }
+                }
+            }
+        }
+
+        return default;
+    }
+
+}
