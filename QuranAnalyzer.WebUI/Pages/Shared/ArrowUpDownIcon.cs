bcdf2638f1b339e4b99e1ea5943372d7cd590dba
beyaz <beyaz1404@gmail.com>
2025-01-25 00:00:08 +0300

    .

--- QuranAnalyzer.WebUI/Pages/Shared/ArrowUpDownIcon.cs
+++ QuranAnalyzer.WebUI/Pages/Shared/ArrowUpDownIcon.cs
@@ -6,39 +6,11 @@ class ArrowUpDownIcon : ReactPureComponent
 
     protected override Element render()
     {
-        var rotationStyle = new style
-        {
-            """
-            .transform_rotate_negative_180deg
-            {
-               transform: rotate(-180deg);
-            }
-
-            .transform_rotate_90deg
-            {
-               transform: rotate(0deg);
-            }
-            """
-        };
-
         var arrowDown = new svg(svg.ViewBox(0, 0, 24, 24), svg.Size(24))
         {
             new path { d = "M8.12 9.29 12 13.17l3.88-3.88c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41l-4.59 4.59c-.39.39-1.02.39-1.41 0L6.7 10.7a.9959.9959 0 0 1 0-1.41c.39-.38 1.03-.39 1.42 0z" }
         };
 
-        if (IsArrowUp)
-        {
-            return new Fragment
-            {
-                rotationStyle,
-                arrowDown + Transition("all", 400) + ClassName("transform_rotate_negative_180deg")
-            };
-        }
-
-        return new Fragment
-        {
-            rotationStyle,
-            arrowDown + Transition("all", 400)+ ClassName("transform_rotate_90deg")
-        };
+        return arrowDown + Transition("all", 400) + Transform(IsArrowUp ? "rotate(-180deg)" : "rotate(0deg)");
     }
 }
