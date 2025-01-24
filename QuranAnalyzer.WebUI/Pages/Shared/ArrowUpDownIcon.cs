75c5bbf11fb78f37213d3819e724258583c2808e
beyaz <beyaz1404@gmail.com>
2025-01-24 23:14:51 +0300

    .

--- QuranAnalyzer.WebUI/Pages/Shared/ArrowUpDownIcon.cs
+++ QuranAnalyzer.WebUI/Pages/Shared/ArrowUpDownIcon.cs
@@ -21,7 +21,7 @@ class ArrowUpDownIcon : ReactPureComponent
             """
         };
 
-        var arrowDown = new svg(svg.ViewBox(0, 0, 24, 24), svg.Size(24), Transition("all", 400))
+        var arrowDown = new svg(svg.ViewBox(0, 0, 24, 24), svg.Size(24))
         {
             new path { d = "M8.12 9.29 12 13.17l3.88-3.88c.39-.39 1.02-.39 1.41 0 .39.39.39 1.02 0 1.41l-4.59 4.59c-.39.39-1.02.39-1.41 0L6.7 10.7a.9959.9959 0 0 1 0-1.41c.39-.38 1.03-.39 1.42 0z" }
         };
@@ -31,14 +31,14 @@ class ArrowUpDownIcon : ReactPureComponent
             return new Fragment
             {
                 rotationStyle,
-                arrowDown + ClassName("transform_rotate_negative_180deg")
+                arrowDown + Transition("all", 400) + ClassName("transform_rotate_negative_180deg")
             };
         }
 
         return new Fragment
         {
             rotationStyle,
-            arrowDown + ClassName("transform_rotate_90deg")
+            arrowDown + Transition("all", 400)+ ClassName("transform_rotate_90deg")
         };
     }
 }
