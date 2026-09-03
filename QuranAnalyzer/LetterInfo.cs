b4405d82ba3dcc2a21e4a8e7ae18dc04553728dc
Abdullah Beyaztaş <beyaz1404@gmail.com>
2026-09-03 22:59:01 +0300

    .

--- QuranAnalyzer/LetterInfo.cs
+++ QuranAnalyzer/LetterInfo.cs
@@ -8,6 +8,20 @@ public sealed record LetterInfo
 
     public int OrderValue { get; init; }
 
+    public override string ToString()
+    {
+        return Letter.ToString();
+    }
+}
+
+public sealed record LetterWithLocationInfo
+{
+    public char Letter { get; init; }
+
+    public int NumericValue { get; init; }
+
+    public int OrderValue { get; init; }
+
     public int StartIndex { get; init; }
 
     public override string ToString()
