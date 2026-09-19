10dc1874fc6e246c7cbd02f707d0245db45f13ea
Abdullah Beyaztaş <beyaz1404@gmail.com>
2026-09-19 16:04:03 +0300

    .

--- QuranAnalyzer.WebUI/Pages/PageWordSearching/WordColorizedVerse.cs
+++ QuranAnalyzer.WebUI/Pages/PageWordSearching/WordColorizedVerse.cs
@@ -5,29 +5,29 @@ namespace QuranAnalyzer.WebUI.Pages.PageWordSearching;
 
 sealed record WordColorizedVerseModelItem
 {
-    public required string Word { get; init; }
-    
     public required string Color { get; init; }
 
     public required int Count { get; init; }
-
+    public required string Word { get; init; }
 }
+
 sealed record WordColorizedVerseModel
 {
     public required string HtmlString { get; init; }
-    
+
     public required IReadOnlyList<WordColorizedVerseModelItem> Words { get; init; }
 }
+
 sealed class WordColorizedVerse : ReactPureComponent
 {
     public required IReadOnlyList<(IReadOnlyList<LetterInfo> searchWord, IReadOnlyList<(LetterInfo start, LetterInfo end)> startEndPoints)> MatchList { get; set; }
-    
+
     public required Verse Verse { get; init; }
 
-    internal static WordColorizedVerseModel Calculate( Verse Verse, IReadOnlyList<(IReadOnlyList<LetterInfo> searchWord, IReadOnlyList<(LetterInfo start, LetterInfo end)> startEndPoints)> MatchList )
+    internal static WordColorizedVerseModel Calculate(Verse Verse, IReadOnlyList<(IReadOnlyList<LetterInfo> searchWord, IReadOnlyList<(LetterInfo start, LetterInfo end)> startEndPoints)> MatchList)
     {
         List<WordColorizedVerseModelItem> words = [];
-            
+
         var verseLetters = Verse.TextAnalyzed.ToList();
 
         var cursor = 0;
@@ -88,152 +88,64 @@ sealed class WordColorizedVerse : ReactPureComponent
             cursor++;
         }
 
-
         {
             var searchWordIndex = 0;
 
             foreach (var (searchWord, startEndPoints) in MatchList)
             {
-                words.Add(new ()
+                words.Add(new()
                 {
-                    Word = string.Join(string.Empty, searchWord),
+                    Word  = string.Join(string.Empty, searchWord),
                     Color = GetColor(searchWordIndex),
                     Count = startEndPoints.Count
                 });
-                
-               
 
                 searchWordIndex++;
             }
         }
-        
-
-       
 
         return new()
         {
             HtmlString = html.ToString(),
-            
+
             Words = words
         };
     }
 
     protected override Element render()
     {
-        var verseLetters = Verse.TextAnalyzed.ToList();
+        var model = Calculate(Verse, MatchList);
 
-        var cursor = 0;
-
-        var html = new StringBuilder();
-
-        while (cursor < verseLetters.Count)
+        return new fieldset(DisplayFlex, FlexDirectionColumn, AlignItemsFlexEnd, Border(1, dashed, rgb(218, 220, 224)), BorderRadiusForPanels)
         {
-            var letterInfo = verseLetters[cursor];
-
-            var hasAnyMatch = false;
-
-            var searchWordIndex = 0;
-            foreach (var (_, startEndPoints) in MatchList)
+            new legend(DisplayFlex, FlexDirectionRow, AlignItemsCenter, Gap(5), UserSelect(none))
             {
-                foreach (var startEndPoint in startEndPoints)
+                new div(FontWeightBold, MarginLeft(2), FontSize13)
                 {
-                    if (startEndPoint.start == letterInfo)
+                    $"{Verse.Id}"
+                },
+                new FlexRow(FlexWrap, JustifyContentCenter, Padding(5), Gap(13))
+                {
+                    from item in model.Words
+                    select new FlexRow(AlignItemsCenter)
                     {
-                        var endIndex = verseLetters.IndexOf(startEndPoint.end, cursor);
+                        new div { item.Word, FontWeightBold, Color(item.Color) },
 
-                        var span = new span
-                        {
-                            innerText = verseLetters.GetRange(cursor, endIndex - cursor + 1).AsText(),
-                            style =
-                            {
-                                FontWeightBold,
-                                BorderRadiusForPanels,
-                                Border("1px dashed rgb(218, 220, 224)"),
-                                Color(GetColor(searchWordIndex))
-                            }
-                        };
+                        new div { ":", MarginLeftRight(4) },
 
-                        html.Append(span.ToHtml());
-
-                        cursor = endIndex + 1;
-
-                        hasAnyMatch = true;
-                        break;
+                        new div { item.Count.ToString(), FontSize12 }
                     }
                 }
-
-                if (hasAnyMatch)
+            },
+            new div(FontFamily_Lateef)
+            {
+                innerHTML = model.HtmlString,
+                style =
                 {
-                    break;
+                    FontSize(38),
+                    Padding(5),
+                    DirectionRtl
                 }
-
-                searchWordIndex++;
-            }
-
-            if (hasAnyMatch)
-            {
-                continue;
-            }
-
-            html.Append(letterInfo.Letter);
-
-            cursor++;
-        }
-
-        var countsView = new FlexRow(FlexWrap, JustifyContentCenter, Padding(5), Gap(13));
-
-        {
-            var searchWordIndex = 0;
-
-            foreach (var (searchWord, startEndPoints) in MatchList)
-            {
-                var countView = new FlexRow(AlignItemsCenter)
-                {
-                    new div { string.Join(string.Empty, searchWord), FontWeightBold, Color(GetColor(searchWordIndex)) },
-
-                    new div { ":", MarginLeftRight(4) },
-
-                    new div { startEndPoints.Count.ToString(), FontSize12 }
-                };
-
-                countsView.children.Add(countView);
-
-                searchWordIndex++;
-            }
-        }
-        var textView = new div(FontFamily_Lateef)
-        {
-            innerHTML = html.ToString(),
-            style =
-            {
-                FontSize(38),
-                Padding(5),
-                DirectionRtl
-            }
-        };
-
-        var verseId = new div(FontWeightBold, MarginLeft(2), FontSize13)
-        {
-            $"{Verse.Id}"
-        };
-
-        var topLegend = new legend(DisplayFlex, FlexDirectionRow, AlignItemsCenter, Gap(5), UserSelect(none))
-        {
-            verseId,
-            countsView
-        };
-
-        return new fieldset
-        {
-            children = { topLegend, textView },
-            style =
-            {
-                DisplayFlex,
-                FlexDirectionColumn,
-                AlignItemsFlexEnd,
-
-                Border("1px dashed rgb(218, 220, 224)"),
-                BorderRadiusForPanels
             }
         };
     }
